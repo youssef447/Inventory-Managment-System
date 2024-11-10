ecacd0f5f613f02b00f1d2a85c91fd7d8b52a69b
youssef447 <youssef.ashraf380@gmail.com>
2024-11-10 22:07:42 +0200

    updated asset controller

--- lib/features/Assets/presentation/controller/assets_controller.dart
+++ lib/features/Assets/presentation/controller/assets_controller.dart
@@ -28,80 +28,82 @@ class AssetsController extends GetxController {
   TextEditingController quantityController = TextEditingController();
   TextEditingController dateReceivedController = TextEditingController();
   TextEditingController dateReturnController = TextEditingController();
-  TextEditingController maintenanceFrequencyController = TextEditingController();
-  TextEditingController nextMaintenanceScheduleController = TextEditingController();
+  TextEditingController maintenanceFrequencyController =
+      TextEditingController();
+  TextEditingController nextMaintenanceScheduleController =
+      TextEditingController();
   TextEditingController expirationDateController = TextEditingController();
   TextEditingController statusController = TextEditingController();
 // show dummy data for test ui ------ will be removed --------
   Future<void> loadAssetsData() async {
-    Future.delayed(
+    await Future.delayed(
       const Duration(seconds: 2),
-    ).then((_) {
-      assetsList = [
-        AssetsEntity(
-          assetId: 'A101',
+    );
+    assetsList = [
+      AssetsEntity(
+        assetId: 'A101',
+        assetName: 'Laptop',
+        category: 'Electronics',
+        subcategory: 'Computers',
+        model: 'GZ 15',
+        brand: 'dell',
+        dateReceived: DateTime(2023, 1, 10, 9, 0),
+        dateReturn: DateTime(2023, 1, 10, 9, 0),
+        quantity: '9',
+        maintenanceFrequency: 'Monthly',
+        nextMaintenanceSchedule: null,
+        expirationDate: DateTime(2023, 1, 10, 9, 0),
+        status: Status.maintenance,
+      ),
+      AssetsEntity(
+          assetId: 'A102',
           assetName: 'Laptop',
           category: 'Electronics',
           subcategory: 'Computers',
           model: 'GZ 15',
           brand: 'dell',
           dateReceived: DateTime(2023, 1, 10, 9, 0),
-          dateReturn: DateTime(2023, 1, 10, 9, 0),
-          quantity: '9',
-          maintenanceFrequency: 'Monthly',
+          dateReturn: null,
+          quantity: '10',
+          maintenanceFrequency: null,
           nextMaintenanceSchedule: null,
+          expirationDate: null,
+          status: Status.returned),
+      AssetsEntity(
+          assetId: 'A103',
+          assetName: 'Laptop',
+          category: 'Electronics',
+          subcategory: 'Computers',
+          model: 'HY-15',
+          brand: 'dell',
+          dateReceived: DateTime(2023, 1, 10, 9, 0),
+          dateReturn: null,
+          quantity: '5',
+          maintenanceFrequency: 'Monthly',
+          nextMaintenanceSchedule: DateTime(2023, 1, 10, 9, 0),
           expirationDate: DateTime(2023, 1, 10, 9, 0),
-          status: Status.maintenance,
-        ),
-        AssetsEntity(
-            assetId: 'A102',
-            assetName: 'Laptop',
-            category: 'Electronics',
-            subcategory: 'Computers',
-            model: 'GZ 15',
-            brand: 'dell',
-            dateReceived: DateTime(2023, 1, 10, 9, 0),
-            dateReturn: null,
-            quantity: '10',
-            maintenanceFrequency: null,
-            nextMaintenanceSchedule: null,
-            expirationDate: null,
-            status: Status.returned),
-        AssetsEntity(
-            assetId: 'A103',
-            assetName: 'Laptop',
-            category: 'Electronics',
-            subcategory: 'Computers',
-            model: 'HY-15',
-            brand: 'dell',
-            dateReceived: DateTime(2023, 1, 10, 9, 0),
-            dateReturn: null,
-            quantity: '5',
-            maintenanceFrequency: 'Monthly',
-            nextMaintenanceSchedule: DateTime(2023, 1, 10, 9, 0),
-            expirationDate: DateTime(2023, 1, 10, 9, 0),
-            status: Status.inUse),
-        // Add more assets as needed
-      ];
-      if (assetsList.isNotEmpty) {
-        final asset = assetsList[0]; // Using the first asset as an example
-        assetIdController.text = asset.assetId;
-        assetNameController.text = asset.assetName;
-        categoryController.text = asset.category;
-        subcategoryController.text = asset.subcategory;
-        modelController.text = asset.model;
-        brandController.text = asset.brand;
-        quantityController.text = asset.quantity;
-        dateReceivedController.text = DateTimeHelper.formatDate(asset.dateReceived);
-        dateReturnController.text = DateTimeHelper.formatDate(asset.dateReturn!);
-        maintenanceFrequencyController.text = asset.maintenanceFrequency ?? '';
-        nextMaintenanceScheduleController.text =
-            asset.nextMaintenanceSchedule?.toString() ?? '';
-        expirationDateController.text = asset.expirationDate?.toString() ?? '';
-        statusController.text = asset.status.getName;
-      }
-      loading = false;
-      update([AssetsIdConstant.assetsData]);
-    });
+          status: Status.inUse),
+      // Add more assets as needed
+    ];
+    if (assetsList.isNotEmpty) {
+      final asset = assetsList[0]; // Using the first asset as an example
+      assetIdController.text = asset.assetId;
+      assetNameController.text = asset.assetName;
+      categoryController.text = asset.category;
+      subcategoryController.text = asset.subcategory;
+      modelController.text = asset.model;
+      brandController.text = asset.brand;
+      quantityController.text = asset.quantity;
+      dateReceivedController.text =
+          DateTimeHelper.formatDate(asset.dateReceived);
+      dateReturnController.text = DateTimeHelper.formatDate(asset.dateReturn!);
+      maintenanceFrequencyController.text = asset.maintenanceFrequency ?? '';
+      nextMaintenanceScheduleController.text =
+          asset.nextMaintenanceSchedule?.toString() ?? '';
+      expirationDateController.text = asset.expirationDate?.toString() ?? '';
+      statusController.text = asset.status.getName;
+    }
+    loading = false;
+    update([AssetsIdConstant.assetsData]);
   }
 }
